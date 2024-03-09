13525b11b06e221e3afce2e1f323271f2be1fdc4
Aditya Singh <sengaraditya01@gmail.com>
2024-03-09 11:36:02 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Currency Converter 
+Using React
+
+Things I learnt from this project.
+
+1. How to effectively use useState hook.
+2. How to use useEffect hook.
+3. How to make a custom hook.
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,50 @@
+import React,{useId} from "react";
+
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOption = [],
+  selectCurrency = "usd",
+  className = "",
+  amountDisable = false,
+  currencyDisable = false,
+}) {
+    const amountInputId = useId();
+  return (
+    <div className={` p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label className="text-black/40 mb-2 inline-block" htmlFor={amountInputId}>{label}</label>
+
+        <input
+            id={amountInputId}
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) => {
+            onAmountChange && onAmountChange(Number(e.target.value));
+          }}
+        />
+      </div>
+
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          value={selectCurrency}
+          className="rounded-lg px-1 py-1 bg-gray-100 curser-pointer outline-none"
+          disabled={currencyDisable}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+            {currencyOption.map((currency)=>(
+                <option key={currency} value={currency}>{currency}</option>
+            ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
